6908f5602bda82c9defdd4de663c092f75d0e3ef
Zuraiz Zafar <zuraiz.zafar97@gmail.com>
2017-11-12 01:33:44 -0500

    partial

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
-import {Calendar} from "./Calendar"
+import Calendar from "./Calendar";
 import './App.css';
+import Courses from  './schedules/schedule_mini.json';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Calendar />
+      <Calendar courses={Courses.courses} />
       </div>
     );
   }
